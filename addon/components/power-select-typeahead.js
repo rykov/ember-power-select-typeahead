c6c76a98ce94fe80ec5b8b74db5da33ce0a3769b
Michael Rykov <mrykov@gmail.com>
2024-04-24 00:17:55 +0800

    Migrated to Glimmer and native classes

--- addon/components/power-select-typeahead.js
+++ addon/components/power-select-typeahead.js
@@ -1,49 +1,53 @@
-import Component from '@ember/component';
-import { computed } from '@ember/object';
 import layout from '../templates/components/power-select-typeahead';
+import Component from '@glimmer/component';
+import { action } from '@ember/object';
 
-export default Component.extend({
-  tagName: '',
-  layout,
-  tabindex: -1,
-  triggerComponent: 'power-select-typeahead/trigger',
-  beforeOptionsComponent: null,
-  loadingMessage: null,
-  noMatchesMessage: null,
-  onkeydown: () => {},
+const noopKeyDown = () => {};
+
+export default class PowerSelectTypeahead extends Component {
+  layout = layout;
+
+  // Args w/ defaults
+  constructor() {
+    super(...arguments);
+    this.triggerComponent = this.args.triggerComponent;
+    this.triggerComponent ||= 'power-select-typeahead/trigger';
+    this.beforeOptionsComponent = this.args.beforeOptionsComponent || null;
+    this.noMatchesMessage = this.args.noMatchesMessage || null;
+    this.loadingMessage = this.args.loadingMessage || null;
+    this.tabindex = this.args.tabindex || -1;
+  }
 
   // CPs
-  concatenatedTriggerClasses: computed('triggerClass', function() {
+  get concatenatedTriggerClasses() {
     let classes = ['ember-power-select-typeahead-trigger'];
-    let passedClass = this.get('triggerClass');
+    let passedClass = this.args.triggerClass;
     if (passedClass) {
       classes.push(passedClass);
     }
     return classes.join(' ');
-  }),
+  }
 
-  concatenatedDropdownClasses: computed('dropdownClass', function() {
+  get concatenatedDropdownClasses() {
     let classes = ['ember-power-select-typeahead-dropdown'];
-    let passedClass = this.get('dropdownClass');
+    let passedClass = this.args.dropdownClass;
     if (passedClass) {
       classes.push(passedClass);
     }
     return classes.join(' ');
-  }),
+  }
 
-  actions: {
-    onKeyDown(select, e) {
-      let action = this.get('onkeydown');
+  @action onKeyDown(select, e) {
+    let action = this.args.onkeydown || noopKeyDown;
 
-      // if user passes `onkeydown` action
-      if (action && action(select, e) === false) {
-        return false;
-      } else {
-        // if escape, then clear out selection
-        if (e.keyCode === 27) {
-          select.actions.choose(null);
-        }
+    // if user passes `onkeydown` action
+    if (action && action(select, e) === false) {
+      return false;
+    } else {
+      // if escape, then clear out selection
+      if (e.keyCode === 27) {
+        select.actions.choose(null);
       }
     }
   }
-});
+}
--- addon/components/power-select-typeahead/trigger.js
+++ addon/components/power-select-typeahead/trigger.js
@@ -1,25 +1,36 @@
-import Component from '@ember/component';
-import { isBlank } from '@ember/utils';
-import { schedule } from '@ember/runloop';
 import layout from '../../templates/components/power-select-typeahead/trigger';
+import { addObserver, removeObserver } from '@ember/object/observers';
+import { action, get, set } from '@ember/object';
+import Component from '@glimmer/component';
+import { schedule } from '@ember/runloop';
+import { isBlank } from '@ember/utils';
 
-export default Component.extend({
-  layout,
-  tagName: '',
-  text: '',
+export default class PowerSelectTypeaheadTrigger extends Component {
+  layout = layout;
+  text = '';
+
+  constructor() {
+    super(...arguments);
+    addObserver(this, 'args.select', this, 'onUpdatedAttrs');
+    this.onUpdatedAttrs();
+  }
+
+  willDestroy() {
+    super.willDestroy(...arguments);
+    removeObserver(this, 'args.select', this, 'onUpdatedAttrs');
+  }
 
   /**
-   * Lifecycle Hook
+   * Pseudo-Lifecycle Hook
    * power-select updates the state of the publicAPI (select) for every typeahead
    * so we capture this as `state` via oldSelect && newSelect
    *
    * @private
-   * @method didReceiveAttrs
+   * @method onUpdatedAttrs
    */
-  didReceiveAttrs() {
-    this._super(...arguments);
-    let oldSelect = this.get('oldSelect');
-    let newSelect = this.set('oldSelect', this.get('select'));
+  @action onUpdatedAttrs() {
+    let oldSelect = this.oldSelect;
+    let newSelect = (this.oldSelect = get(this, 'args.select'));
     if (!oldSelect) {
       return;
     }
@@ -28,12 +39,14 @@ export default Component.extend({
      * the select box.
      */
     if (oldSelect.isOpen && !newSelect.isOpen && newSelect.searchText) {
-      let input = document.querySelector(`#ember-power-select-typeahead-input-${newSelect.uniqueId}`);
+      let input = document.querySelector(
+        `#ember-power-select-typeahead-input-${newSelect.uniqueId}`
+      );
       let newText = this.getSelectedAsText();
       if (input.value !== newText) {
         input.value = newText;
       }
-      this.set('text', newText);
+      set(this, 'text', newText);
     }
 
     if (newSelect.lastSearchedText !== oldSelect.lastSearchedText) {
@@ -45,53 +58,51 @@ export default Component.extend({
     }
 
     if (oldSelect.selected !== newSelect.selected) {
-      this.set('text', this.getSelectedAsText());
+      set(this, 'text', this.getSelectedAsText());
     }
-  },
+  }
 
-  actions: {
-    /**
-     * on mousedown prevent propagation of event
-     *
-     * @private
-     * @method stopPropagation
-     * @param {Object} event
-     */
-    stopPropagation(e) {
+  /**
+   * on mousedown prevent propagation of event
+   *
+   * @private
+   * @method stopPropagation
+   * @param {Object} event
+   */
+  @action stopPropagation(e) {
+    e.stopPropagation();
+  }
+
+  /**
+   * called from power-select internals
+   *
+   * @private
+   * @method handleKeydown
+   * @param {Object} event
+   */
+  @action handleKeydown(e) {
+    // up or down arrow and if not open, no-op and prevent parent handlers from being notified
+    if ([38, 40].includes(e.keyCode) && !get(this, 'args.select.isOpen')) {
       e.stopPropagation();
-    },
-
-    /**
-     * called from power-select internals
-     *
-     * @private
-     * @method handleKeydown
-     * @param {Object} event
-     */
-    handleKeydown(e) {
-      // up or down arrow and if not open, no-op and prevent parent handlers from being notified
-      if ([38, 40].indexOf(e.keyCode) > -1 && !this.get('select.isOpen')) {
-        e.stopPropagation();
-        return;
-      }
-      let isLetter = e.keyCode >= 48 && e.keyCode <= 90 || e.keyCode === 32; // Keys 0-9, a-z or SPACE
-      // if isLetter, escape or enter, prevent parent handlers from being notified
-      if (isLetter || [13, 27].indexOf(e.keyCode) > -1) {
-        let select = this.get('select');
-        // open if loading msg configured
-        if (!select.isOpen && this.get('loadingMessage')) {
-          schedule('actions', null, select.actions.open);
-        }
-        e.stopPropagation();
-      }
-
-      // optional, passed from power-select
-      let onkeydown = this.get('onKeydown');
-      if (onkeydown && onkeydown(e) === false) {
-        return false;
-      }
+      return;
     }
-  },
+    let isLetter = (e.keyCode >= 48 && e.keyCode <= 90) || e.keyCode === 32; // Keys 0-9, a-z or SPACE
+    // if isLetter, escape or enter, prevent parent handlers from being notified
+    if (isLetter || [13, 27].includes(e.keyCode)) {
+      let select = get(this, 'args.select');
+      // open if loading msg configured
+      if (!select.isOpen && get(this, 'args.loadingMessage')) {
+        schedule('actions', null, select.actions.open);
+      }
+      e.stopPropagation();
+    }
+
+    // optional, passed from power-select
+    let onkeydown = get(this, 'args.onKeydown');
+    if (onkeydown && onkeydown(e) === false) {
+      return false;
+    }
+  }
 
   /**
    * obtains seleted value based on complex object or primitive value from power-select publicAPI
@@ -100,18 +111,8 @@ export default Component.extend({
    * @method getSelectedAsText
    */
   getSelectedAsText() {
-    let labelPath = this.get('extra.labelPath');
-    let value;
-    if (labelPath) {
-      // complex object
-      value = this.get(`select.selected.${labelPath}`);
-    } else {
-      // primitive value
-      value = this.get('select.selected');
-    }
-    if (value === undefined) {
-      value = '';
-    }
-    return value;
+    let path = get(this, 'args.extra.labelPath');
+    let value = get(this, `args.select.selected${path ? `.${path}` : ''}`);
+    return value === undefined ? '' : value;
   }
-});
+}
