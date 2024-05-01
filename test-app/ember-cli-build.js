3168578128b58c3aa8ca04cca90e61576e1fde6d
Michael Rykov <mrykov@gmail.com>
2024-05-01 11:54:35 +0900

    Addon to v2

---
+++ test-app/ember-cli-build.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const EmberApp = require('ember-cli/lib/broccoli/ember-app');
+
+module.exports = function (defaults) {
+  let app = new EmberApp(defaults, {
+    autoImport: {
+      watchDependencies: ['ember-power-select-typeahead'],
+    },
+    babel: {
+      plugins: [
+        require.resolve('ember-concurrency/async-arrow-task-transform'),
+      ],
+    },
+    'ember-cli-babel': { enableTypeScriptTransform: true },
+  });
+
+  const { maybeEmbroider } = require('@embroider/test-setup');
+  return maybeEmbroider(app);
+};
